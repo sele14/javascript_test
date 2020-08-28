88ee66f59c0578f987fd6d1cc3cbe4a92fd8f46f
sele14 <eirik.selander@gmail.com>
2020-08-28 21:06:08 +0100

    JS test

---
+++ custom.js
@@ -0,0 +1,12 @@
+console.log("Hello world")
+
+var number = 5;
+console.log(number)
+
+function num_function(num){
+    if(num > 1){
+        return console.log("The number was greater than 1.")
+    }
+}
+
+num_function(number)
